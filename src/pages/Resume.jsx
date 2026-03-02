22e24a6c07f5dda83a55f27564159c31644386ca
javier garcia alvarez <javigarcial87@gmail.com>
2026-03-02 15:42:19 +0100

    enlace resume

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,7 +1,32 @@
 import {studies, experiences} from "../data/resume"
 
 function Resume() {
-  return <h1>Resume</h1>;
+  return (
+    <div>
+      <h1></h1>
+
+      <h2></h2>
+      {experiences.map(exp => (
+        <div key={exp.id}>
+          <h3>{exp.title}</h3>
+          <p>{exp.company}</p>
+          <p>{exp.date}</p>
+        </div>
+
+      ))}
+
+      <h2></h2>
+       {studies.map(study => (
+        <div key={study.id}>
+          <h3>{study.title}</h3>
+          <p>{study.institution}</p>
+          <p>{study.date}</p>
+        </div>
+
+      ))}
+
+    </div>
+  )
 }
 
 
