8b281b7f45bdb70e362d0669f28367187796924c
javier garcia alvarez <javigarcial87@gmail.com>
2026-03-02 14:34:12 +0100

    Home

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,5 +1,15 @@
+import { Link} from "react-router-dom"
+
 function Home() {
-  return <h1>Home</h1>;
+  return (
+    <div>
+      <h1>Bienvenido a mi portfolio</h1>
+      <p>Soy desarrollador web fullStack</p>
+
+      <Link to="/projects" ></Link>
+      <Link to="/resume" ></Link>
+    </div>
+  )
 }
 
 
