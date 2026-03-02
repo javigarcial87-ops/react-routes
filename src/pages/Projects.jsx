a99d222e9e69407748a6dc15a3a5064add8432cf
javier garcia alvarez <javigarcial87@gmail.com>
2026-03-02 15:17:14 +0100

    enlace de rutas

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,5 +1,20 @@
-function Projects() {
-  return <h1>Projects</h1>;
+import projects from "../data/projects"
+
+function Projects() {  
+  return (
+    <div>
+      <h1>Mis Proyectos</h1>
+      {projects.map(project => (
+        <div key={project.id}>
+          <h2>{project.name}</h2>
+          <img src="{project.image}" alt="{project.name}" width="200" />
+          <p>{project.description}</p>
+          <a href="{project.url}" target="_blank">Ver proyecto</a>
+        </div>
+      ))}
+    </div>
+    
+  ) 
 }
 
 
