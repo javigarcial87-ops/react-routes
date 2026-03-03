10c17c6e29774d89dfc4f97a8c8596d887d567b9
javier garcia alvarez <javigarcial87@gmail.com>
2026-03-03 10:39:20 +0100

    correcion de tipado

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -7,9 +7,9 @@ function Projects() {
       {projects.map(project => (
         <div key={project.id}>
           <h2>{project.name}</h2>
-          <img src="{project.image}" alt="{project.name}" width="200" />
+          <img src={project.image} alt="{project.name}" width="200" />
           <p>{project.description}</p>
-          <a href="{project.url}" target="_blank">Ver proyecto</a>
+          <a href={project.url} target="_blank">Ver proyecto</a>
         </div>
       ))}
     </div>
